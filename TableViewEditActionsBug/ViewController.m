751f722c69b317ea07b5533006db0c6704191ac4
Bill Bonney <bill@communistech.com>
2017-09-09 16:57:38 -0700

    1st commit

--- TableViewEditActionsBug/ViewController.m
+++ TableViewEditActionsBug/ViewController.m
@@ -10,19 +10,52 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NSArray* data;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _data = @[@"Thing 1", @"Thing 2", @"Thing 3", @"Thing 4", @"Thing 5", @"Thing 6"];
+
 }
 
+-(void)viewDidAppear:(BOOL)animated {
+    [self.tableView reloadData];
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _data.count;
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Enable 'Delete' edit Action for odd cells
+    return (indexPath.row % 2)? UITableViewCellEditingStyleNone : UITableViewCellEditingStyleDelete;
+}
+
+- (BOOL)tableView:(UITableView*)tableView canEditRowAtIndexPath:(NSIndexPath*)indexPath {
+    return YES;
+}
+
+- (NSArray *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewRowAction *deleteAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDefault
+                                                                            title:NSLocalizedString(@"Delete",@"")
+                                                                          handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+                                                                              NSLog(@"Delete Me");
+                                                                          }];
+    return @[deleteAction];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *MyIdentifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+    cell.textLabel.text = _data[indexPath.row];
+    return cell;
+    
 }
 
 
